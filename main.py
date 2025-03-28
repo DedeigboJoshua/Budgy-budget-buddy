194635ace4ebd6924fc8f15ea71561620600c863
DedeigboJoshua <dedeigbojoshua@gmail.com>
2025-03-27 23:14:43 -0400

    Update main.py
    Creating some of the main functions the program will use. Might get rid of the idea of partitions as i don't know how it'll work

--- main.py
+++ main.py
@@ -12,8 +12,34 @@
   
     View Account: Display the account with the total amount and goals/budgets(potentially both as dollar amounts and as percentages of the total)"""
 
-user_base = []
+# dictionary created as a place to store users. Might consider using a random number generator and associate a user's chosen name with the number
+user_base = {}
 
 
 class User:
-    def __init__(self, amount, )
+    def __init__(self, account_total):
+        # dictionary of goals to be updated with the user
+        self.goals = {}
+        
+        self.total = account_total
+
+    def add_goal(self, goal_name, goal_amount):
+        self.goal_name = goal_name
+        self.goal_amount = goal_amount
+        
+        # updating goal dictionary
+        self.goals[goal_name] = goal_amount
+
+    def edit_goal(self, goal_name, new_goal_amount):
+        if self.goal_name in self.goals:
+            self.goals[goal_name] = new_goal_amount
+        else:
+            print(f"Goal '{goal_name}' does not exist.")
+    
+    def delete_goal(self, goal_name):
+        if self.goal_name in self.goals:
+            del self.goals[goal_name]
+        else:
+            print(f"Goal '{self.goal_name}' does not exist")
+    
+    
